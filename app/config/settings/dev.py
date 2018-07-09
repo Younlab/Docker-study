7c3404afb208cd81b45d38eb838ef3c830c33808
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-09 23:30:16 +0900

    S3 를 위한 boto3 pipenv install

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -1,1 +1,20 @@
 # 디버그 메세지는 출력하되 외부 서버를 사용
+from .base import *
+
+DEBUG = True
+
+# Static
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
+STATIC_URL = '/static/'
+
+# Media
+MEDIA_ROOT = os.path.join(ROOT_DIR,'.media')
+MEDIA_URL = '/media/'
+
+# uWSGI
+WSGI_APPLICATION = 'config.wsgi.application'
+
+secret_db = json.load(open(os.path.join(SECRET_DIR, 'db.json')))
+
+# DB
+DATABASES = secret_db['DATABASES']
